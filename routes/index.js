8676ee104333aa3910dfe736ad6f26f44a7de57a
Veronique Azzopardi <azzopardivero@gmail.com>
2023-11-03 15:14:51 +0100

    init

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/date", (req, res) => {
+  const date = new Date.getYear();
+
+  res.json({ now: date });
+});
+
+module.exports = router;
